bea0a62f4c1688403a158220f20fa18f6721f682
SadikHajar <sadik.hajar@etu.uae.ac.ma>
2024-03-14 14:27:27 +0000

    update

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Sadik Hajar</span> I am a student in the engineering cycle specializing in software and intelligent systems, passionate about technology and development. I have developed a strong ability to solve problems and find innovative solutions, as well as my capacity to work in teams. I have a keen interest in new technologies and a strong desire to experiment with new things."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
